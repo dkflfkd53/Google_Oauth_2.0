e5ba690587c3115d6d83bce5a0d722981990c254
dkflfkd53 <b57062562@gmail.com>
2024-09-27 16:29:19 +0900

    user domain 추가

---
+++ src/main/java/com/example/google_oauth_2/user/domain/User.java
@@ -0,0 +1,23 @@
+package com.example.google_oauth_2.user.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+    private String picture;
+    private String nickname;
+}
