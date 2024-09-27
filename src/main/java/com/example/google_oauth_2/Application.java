ff8e22ed202f4d54c6ef67837ad4e5775d26fe3b
dkflfkd53 <b57062562@gmail.com>
2024-09-27 16:58:52 +0900

    Feign class 컴포넌트로 추가

--- src/main/java/com/example/google_oauth_2/Application.java
+++ src/main/java/com/example/google_oauth_2/Application.java
@@ -2,8 +2,10 @@ package com.example.google_oauth_2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class Application {
 
     public static void main(String[] args) {
